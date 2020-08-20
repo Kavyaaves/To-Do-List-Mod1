3d8f659c95883f6047181ebf152fc03ef8bce256
Kavya <kavyaaves2019@gmail.com>
2020-08-20 21:50:55 +0530

    To-Do Model 1

---
+++ src/ListItems.js
@@ -0,0 +1,38 @@
+import "./ListItems.css";
+import React from "react";
+import { fontIcon, FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import FlipMove from "react-flip-move";
+function ListItems(props) {
+  const items = props.items;
+  const listItems = items.map((item) => {
+    return (
+      <div className="list" key={item.key}>
+        <p>
+          <input
+            value={item.text}
+            key={item.key}
+            onChange={(e) => {
+              props.setUpdate(e.target.value, item.key);
+            }}
+          />
+          <span>
+            <FontAwesomeIcon
+              className="faicons"
+              onClick={() => props.deleteItem(item.key)}
+              icon="trash"
+            />
+          </span>
+        </p>
+      </div>
+    );
+  });
+  return (
+    <div>
+      <FlipMove duration={300} easing="ease-in-out">
+        {listItems}
+      </FlipMove>
+    </div>
+  );
+}
+
+export default ListItems;
